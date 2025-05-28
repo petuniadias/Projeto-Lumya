7f7db41c16dd17695d881ac0da46a35937d00cc3
Petúnia <psofia20056@gmail.com>
2025-05-28 16:11:08 +0100

    trip's classes

---
+++ js/models/TripModel.js
@@ -0,0 +1,25 @@
+class Flights {
+  destination = '';
+
+  constructor(destination) {
+    this.destination = destination;
+  }
+}
+
+class Stays {
+  title = '';
+  rating = '';
+
+  constructor(title, rating) {
+    this.title = title;
+    this.rating = rating;
+  }
+}
+
+class Festivals {
+  name = '';
+
+  constructor(name) {
+    this.name = name;
+  }
+}
